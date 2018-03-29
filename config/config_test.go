968f415789b6f15fa311675842b5bc12d944061a
Bao Nguyen Hoai <nhbao.la@gmail.com>
2018-03-29 21:10:58 +0700

    Refactor config test

--- config/config_test.go
+++ config/config_test.go
@@ -35,24 +35,11 @@ func TestLoadConfigFromEnv(t *testing.T) {
 	c, err := config.Load(bytes.NewReader(b))
 
 	assert.NoError(t, err)
-	assert.Equal(t, &config.Config{
-		Server: struct {
-			Addr string
-		}{
-			Addr: serverAddr,
-		},
-		Database: struct {
-			Addr     string
-			User     string
-			Password string
-			Name     string
-		}{
-			Addr:     dbAddr,
-			User:     dbUser,
-			Password: dbPass,
-			Name:     dbName,
-		},
-	}, c)
+	assert.Equal(t, serverAddr, c.Server.Addr)
+	assert.Equal(t, dbAddr, c.Database.Addr)
+	assert.Equal(t, dbUser, c.Database.User)
+	assert.Equal(t, dbPass, c.Database.Password)
+	assert.Equal(t, dbName, c.Database.Name)
 }
 
 func TestLoadConfigFromReader(t *testing.T) {
@@ -69,22 +56,9 @@ database:
 	c, err := config.Load(r)
 
 	assert.NoError(t, err)
-	assert.Equal(t, &config.Config{
-		Server: struct {
-			Addr string
-		}{
-			Addr: ":8080",
-		},
-		Database: struct {
-			Addr     string
-			User     string
-			Password string
-			Name     string
-		}{
-			Addr:     "127.0.0.1:5432",
-			User:     "postgres",
-			Password: "mypostgrespw",
-			Name:     "test",
-		},
-	}, c)
+	assert.Equal(t, ":8080", c.Server.Addr)
+	assert.Equal(t, "127.0.0.1:5432", c.Database.Addr)
+	assert.Equal(t, "postgres", c.Database.User)
+	assert.Equal(t, "mypostgrespw", c.Database.Password)
+	assert.Equal(t, "test", c.Database.Name)
 }
